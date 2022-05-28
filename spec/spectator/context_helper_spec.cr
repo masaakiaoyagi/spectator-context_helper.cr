21f602b3141cfc2b4b087ff868e68b2bcea9ace3
Masaaki Aoyagi <masaaki.aoyagi@gmail.com>
2022-05-28 21:53:16 +0900

    feat: add metadata option
    shared option is not supported yet

--- spec/spectator/context_helper_spec.cr
+++ spec/spectator/context_helper_spec.cr
@@ -1,8 +1,59 @@
 require "../spec_helper"
 
+macro have_metadata(expected)
+  %test_value = Spectator::Value.new({{expected}}, {{expected.stringify}})
+  HaveMetadataMatcher.new(%test_value)
+end
+
+# No custom matcher DSL so far.
+# https://gitlab.com/arctic-fox/spectator/-/issues/65
+struct HaveMetadataMatcher < Spectator::Matchers::StandardMatcher
+  def initialize(@expected : Spectator::Expression(Symbol), @args : Spectator::Mocks::Arguments? = nil)
+  end
+
+  def description : String
+    "TODO"
+  end
+
+  private def match?(actual : Spectator::Expression(T)) : Bool forall T
+    return false unless actual.value.metadata.has_key?(@expected.value)
+    if @args
+      args = to_arguments(actual.value.metadata[@expected.value])
+      return false unless @args === args
+    end
+    true
+  end
+
+  private def failure_message(actual : Spectator::Expression(T)) : String forall T
+    "TODO"
+  end
+
+  def with(*args, **opts)
+    HaveMetadataMatcher.new(@expected, to_arguments(*args, **opts))
+  end
+
+  private def to_arguments(args : Tuple)
+    to_arguments(*args)
+  end
+
+  private def to_arguments(*args, **opts)
+    Spectator::Mocks::GenericArguments.new(args, opts)
+  end
+end
+
 Spectator.describe Spectator::ContextHelper do
   # TODO: unknown how to get 'description'
   # let(:description) {  }
+
+  # does not support shared_context yet
+  # https://gitlab.com/arctic-fox/spectator/-/issues/73
+  # shared_context :s1 do |value = 1|
+  #   let(:v1) { value }
+  # end
+
+  # shared_context :s2 do |value = 2|
+  #   let(:v2) { value }
+  # end
 
   describe ".example_with" do
     example_with           {}
@@ -16,11 +67,31 @@ Spectator.describe Spectator::ContextHelper do
     end
 
     skip "description", reason: "unknown how to get 'description'" do
-      example_with                        { expect(description).to eq "" }
-      example_with "message"              { expect(description).to eq "message" }
-      example_with a: "1"                 { expect(description).to eq %(when a is "1") }
-      example_with a: "1", b: "2"         { expect(description).to eq %(when a is "1" and b is "2") }
-      example_with a: "1", b: "2", c: "3" { expect(description).to eq %(when a is "1", b is "2" and c is "3") }
+      example_with                                            { expect(description).to eq "" }
+      example_with "message"                                  { expect(description).to eq "message" }
+      example_with metadata: :m1                              { expect(description).to eq "" }
+      example_with shared: :s1                                { expect(description).to eq "when s1" }
+      example_with a: "1"                                     { expect(description).to eq %(when a is "1") }
+      example_with a: "1", b: "2"                             { expect(description).to eq %(when a is "1" and b is "2") }
+      example_with metadata: :m1, shared: :s1, a: "1", b: "2" { expect(description).to eq %(when s1, a is "1" and b is "2") }
+    end
+
+    describe "metadata" do
+      example_with metadata: :m1            { |e| expect(e).to have_metadata(:m1).with(nil) }
+      example_with metadata: :m1            { |e| expect(e).not_to have_metadata(:m2) }
+      example_with metadata: {:m1, :m2}     { |e| expect(e).to have_metadata(:m1).with(nil) }
+      example_with metadata: {:m1, :m2}     { |e| expect(e).to have_metadata(:m2).with(nil) }
+      example_with metadata: {:m1, {m2: 3}} { |e| expect(e).to have_metadata(:m1).with(nil) }
+      example_with metadata: {:m1, {m2: 3}} { |e| expect(e).to have_metadata(:m2).with("3") }
+    end
+
+    skip "shared", reason: "does not support shared_context yet" do
+      example_with shared: :s1            { expect(v1).to eq 1 }
+      example_with shared: :s1            { expect{ v2 }.to raise_error NameError }
+      example_with shared: {:s1, :s2}     { expect(v1).to eq 1 }
+      example_with shared: {:s1, :s2}     { expect(v2).to eq 2 }
+      example_with shared: {:s1, {s2: 3}} { expect(v1).to eq 1 }
+      example_with shared: {:s1, {s2: 3}} { expect(v2).to eq 3 }
     end
   end
 
@@ -52,6 +123,14 @@ Spectator.describe Spectator::ContextHelper do
         example { expect(description).to eq "message" }
       end
 
+      context_with metadata: :m1 do
+        example { expect(description).to eq "" }
+      end
+
+      context_with shared: :s1 do
+        example { expect(description).to eq "when s1" }
+      end
+
       context_with a: "1" do
         example { expect(description).to eq %(when a is "1") }
       end
@@ -60,8 +139,42 @@ Spectator.describe Spectator::ContextHelper do
         example { expect(description).to eq %(when a is "1" and b is "2") }
       end
 
-      context_with a: "1", b: "2", c: "3" do
-        example { expect(description).to eq %(when a is "1", b is "2" and c is "3") }
+      context_with metadata: :m1, shared: :s1, a: "1", b: "2" do
+        example { expect(description).to eq %(when s1, a is "1" and b is "2") }
+      end
+    end
+
+    describe "metadata" do
+      context_with metadata: :m1 do
+        example { |e| expect(e).to have_metadata(:m1).with(nil) }
+        example { |e| expect(e).not_to have_metadata(:m2) }
+      end
+
+      context_with metadata: {:m1, :m2} do
+        example { |e| expect(e).to have_metadata(:m1).with(nil) }
+        example { |e| expect(e).to have_metadata(:m2).with(nil) }
+      end
+
+      context_with metadata: {:m1, {m2: 3}} do
+        example { |e| expect(e).to have_metadata(:m1).with(nil) }
+        example { |e| expect(e).to have_metadata(:m2).with("3") }
+      end
+    end
+
+    skip "shared", reason: "does not support shared_context yet" do
+      context_with shared: :s1 do
+        example { expect(v1).to eq 1 }
+        example { expect{ v2 }.to raise_error NameError }
+      end
+
+      context_with shared: {:s1, :s2} do
+        example { expect(v1).to eq 1 }
+        example { expect(v2).to eq 2 }
+      end
+
+      context_with shared: {:s1, {s2: 3}} do
+        example { expect(v1).to eq 1 }
+        example { expect(v2).to eq 3 }
       end
     end
   end
--- src/spectator/context_helper.cr
+++ src/spectator/context_helper.cr
@@ -4,16 +4,48 @@ require "./context_helper/version"
 
 module Spectator::ContextHelper
   module DSL
-    macro example_with(__description__ = nil, **__values__, &block)
-      context_with({{ __description__ }}, {{ **__values__ }}) do
+    macro example_with(__description__ = nil, metadata = nil, shared = nil, **__values__, &block)
+      context_with({{ __description__ }}, {{ metadata }}, {{ shared }}, {{ **__values__ }}) do
         it {{ block }}
       end
     end
 
-    macro context_with(__description__ = nil, **__values__, &block)
-      {% __description__ ||= __values__.to_a.map { |(k, v)| "#{k} is #{v}" }.join(" and ") %}
+    macro context_with(__description__ = nil, metadata = nil, shared = nil, **__values__, &block)
+      {%
+        if metadata.class_name == "TupleLiteral"
+          *rest, last = metadata
+          if last.class_name == "NamedTupleLiteral"
+            tags = rest
+            metadata = last
+          else
+            tags = metadata
+            metadata = {} of Symbol => String
+          end
+        else
+          tags = { metadata }
+          metadata = {} of Symbol => String
+        end
+      %}
 
-      context {{ __description__ }} do
+      {%
+        unless __description__
+          values_words = __values__.to_a.map { |(k, v)| "#{k} is #{v}" }
+          words = values_words
+          size = words.size
+          if size == 0
+            __description__ = ""
+          elsif size == 1
+            __description__ = "when #{words[0]}"
+          elsif size == 2
+            __description__ = "when #{words.join(" and ")}"
+          else
+            *rest, last = words
+            __description__ = "when #{[rest.join(", "), last].join(" and ")}"
+          end
+        end
+      %}
+
+      context {{ __description__ }}, {{ *tags }}, {{ **metadata }} do
         {% for key, value in __values__ %}
           let({{ key }}) do
             {% if value.class_name == "ProcLiteral" %}
